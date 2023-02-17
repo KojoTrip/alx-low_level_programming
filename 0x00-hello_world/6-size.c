f5cac77b70c96f4496d59473ee7f140bb141d0e2
KojoTrip <Jephthahmensah93@gmail.com>
2023-02-16 18:55:16 -0800

    Task 6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - A program that prints the size of various computer types
+ * Return 0 (Success)
+ */
+int main(void)
+{
+        char a;
+        int b;
+        long int c;
+        long long int d;
+        float f;
+
+printf("Size of a char: %lu byte(s)\", (unsigned long)sizeof(a));
+printf("Size of an int: %lu byte(s)nclient_loop: send disconnect: Connection reset by peerze of a long int: %lu byte(s)n\", (unsigned long)sizeof(c));
+printf("Size of a long long int: %lu byte(s)n\", (unsigned long)sizeof(d));
+Administrator@TRIPSOLINO MINGW64 ~s)\n", (unsigned long)sizeof(f));
+return (0);
+}
